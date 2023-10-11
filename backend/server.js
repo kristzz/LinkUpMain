739a393156f1ff2c4d864577ea54da3816066124
kristzz <emils.krists@gmail.com>
2023-10-11 18:09:12 +0300

    removed junk code

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
-
 var bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-// Enable CORS for all routes
 
-
-// Enable preflight requests for all routes
-// app.options('*', cors());
 
 // Create a new pool for the database
 const pool = new Pool({
@@ -23,12 +18,6 @@ const pool = new Pool({
   password: 'Ridzenebasketbal1',
   port: 5432,
 });
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add additional headers if needed
-//   next();
-// });
 
 app.get('/ping', (req, res) =>{
   res.status(200).send('pong')
